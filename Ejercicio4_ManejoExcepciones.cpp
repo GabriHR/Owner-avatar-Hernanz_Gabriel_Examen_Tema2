01c28d4129a86d71b5c4e05e15cc8a19d64e358e
GabriHR <ghernrod@myuax.com>
2023-12-14 13:23:45 +0100

    .

--- Ejercicio4_ManejoExcepciones.cpp
+++ Ejercicio4_ManejoExcepciones.cpp
@@ -17,3 +17,24 @@ void funcionConExcepcion(bool lanzarExcepcion) {
         cout << "La funcion se ejecuto correctamente." << endl;
     }
 }
+
+int main() {
+    try {
+        // Llamada a la función que puede lanzar la excepción
+        funcionConExcepcion(true);
+
+    } catch (const MiExcepcion& e) {
+        // Captura de la excepción personalizada
+        cerr << "Excepcion atrapada: " << e.what() << endl;
+
+    } catch (const exception& e) {
+        // Captura de excepciones genéricas
+        cerr << "Excepcion generica atrapada: " << e.what() << endl;
+
+    } catch (...) {
+        // Captura de cualquier otra excepción no manejada
+        cerr << "Se ha producido una excepcion no manejada." << endl;
+    }
+
+    return 0;
+}
