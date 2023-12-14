87a52c0f5d2c58e3381b2818d4bcdf32e5d7ff1c
GabriHR <ghernrod@myuax.com>
2023-12-14 12:46:14 +0100

    .

--- Ejercicio1_EstructuraEstudiante.c
+++ Ejercicio1_EstructuraEstudiante.c
@@ -14,3 +14,17 @@ void mostrarEstudiante(struct Estudiante estudiante) {
     printf("Promedio: %.2f\n", estudiante.promedio);
 }
 
+int main() {
+    // Crear una instancia de la estructura Estudiante
+    struct Estudiante estudiante1;
+
+    // Asignar valores a los campos de la estructura
+    snprintf(estudiante1.nombre, sizeof(estudiante1.nombre), "Gabriel");
+    estudiante1.edad = 18;
+    estudiante1.promedio = 7.5;
+
+    // Llamar a la función mostrarEstudiante para mostrar la información
+    mostrarEstudiante(estudiante1);
+
+    return 0;
+}
