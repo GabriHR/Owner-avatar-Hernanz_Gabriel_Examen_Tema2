6ee4a9a0364c6d3b07869867d3837873c6ac1a9b
GabriHR <ghernrod@myuax.com>
2023-12-14 13:06:17 +0100

    .

---
+++ Ejercicio3_RegistroAsistencia.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definición de la estructura Asistencia
+struct Asistencia {
+    char fecha[20];
+    char materia[50];
+    char estado[20];
+};
