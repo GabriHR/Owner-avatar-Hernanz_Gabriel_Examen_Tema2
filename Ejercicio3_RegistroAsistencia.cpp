a408b03477b83d72d80b50189e995e7b76cdf72f
GabriHR <ghernrod@myuax.com>
2023-12-14 13:00:22 +0100

    .

--- Ejercicio3_RegistroAsistencia.cpp
+++ Ejercicio3_RegistroAsistencia.cpp
@@ -15,4 +15,29 @@ struct Asistencia {
             : fecha(fecha), materia(materia), estado(estado) {}
 };
 
+// Definicion de la estructura Estudiante
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+    vector<Asistencia> asistencias; // Nuevo miembro para almacenar asistencias
+
+    // Funcion para registrar la asistencia del estudiante
+    void registrarAsistencia(string fecha, string materia, string estado) {
+        Asistencia nuevaAsistencia(fecha, materia, estado);
+        asistencias.push_back(nuevaAsistencia);
+    }
+
+    // Funcion para mostrar la asistencia del estudiante
+    void mostrarAsistencia() const {
+        cout << "Asistencia para el estudiante " << nombre << ":\n";
+
+        for (const Asistencia& asistencia : asistencias) {
+            cout << "Fecha: " << asistencia.fecha << "\n";
+            cout << "Materia: " << asistencia.materia << "\n";
+            cout << "Estado: " << asistencia.estado << "\n\n";
+        }
+    }
+};
+
 
